f490a336ed36412c7be577244b6106d96a1ed653
Shawn Smith <srsmith11@live.com>
2021-05-05 14:57:35 -0600

    added image upload

--- api/app.js
+++ api/app.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 const express = require('express');
 const mongoose = require('mongoose');
+const multer = require('multer');
 
 require('dotenv').config();
 
@@ -11,7 +12,29 @@ const MONGODB_URI = process.env.DATABASE_PATH
 
 const app = express();
 
+const fileStorage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, './images');
+    },
+    filename: (req, file, cb) => {
+        cb(null, new Date().toISOString() + '-' + file.originalname);
+    }
+});
+
+const fileFilter = (req, file, cb) => {
+    if (
+        file.mimetype === 'image/png' || 
+        file.mimetype === 'image/jpg' || 
+        file.mimetype === 'image/jpeg'
+    ) {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    };
+}
+
 app.use(express.json());
+app.use(multer({ storage: fileStorage, fileFilter: fileFilter }).single('image'))
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use((req, res, next) => {
--- api/routes/feed.js
+++ api/routes/feed.js
@@ -8,8 +8,8 @@ const router = express.Router();
 router.get('/posts', feedController.getPosts);
 
 router.post('/post', [
-  body('title').trim().isLength({ min: 5 }),
-  body('content').trim().isLength({ min: 5} ),
+  body('title').isLength({ min: 5 }).trim(),
+  body('content').isLength({ min: 5} ).trim(),
 ], feedController.createPost);
 
 router.get('/post/:postId', [], feedController.getPost);
