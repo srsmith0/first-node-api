3be0d005ff588676a3bc935bf91849b30b9d63fe
Shawn Smith <srsmith11@live.com>
2021-05-04 18:58:31 -0600

    can fetch single post

--- api/controllers/feed.js
+++ api/controllers/feed.js
@@ -1,26 +1,30 @@
-require('express-validator')
+const { validationResult } = require('express-validator')
 
 const Post = require('../models/post');
 
+const handleError = (err, next) => {
+  if (!err.statusCode) {
+    err.statusCode = 500;
+    }
+  next(err);
+}
+
 exports.getPosts = (req, res, next) => {
-  res.status(200).json({
-    posts: [{ 
-      _id:  732989328,
-      title: 'First Post', 
-      content: 'This is the first post!', 
-      imageUrl:  'images/car.jpg',
-      creator: {
-        name: 'Shawn',
-      },
-      createdAt: new Date()
-  }]
+  Post.find()
+  .then(posts => {
+    res.status(200).json({ posts });
+  })
+  .catch(err => {
+    handleError(err, next);
   });
 };
 
 exports.createPost = (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return res.status(422).json({ message: 'Validation failed', errors: errors.array() })
+    const error = new Error(('Validation failed'));
+    error.statusCode = 422
+    throw error;
   }
   const title = req.body.title;
   const content = req.body.content;
@@ -37,5 +41,23 @@ exports.createPost = (req, res, next) => {
       post: result
     });
   })
-  .catch(err => console.log(err))
+  .catch(err => {
+    handleError(err, next)
+  });
+};
+
+exports.getPost = (req, res, next) => {
+  const postId = req.params.postId;
+  Post.findById(postId)
+  .then(post => {
+    if (!post) {
+      const error = new Error('Cound not find post');
+      error.statusCode = 404;
+      throw error;
+    };
+    res.status(200).json({ post });
+  })
+  .catch(err => {
+    handleError(err, next);
+  });
 };
