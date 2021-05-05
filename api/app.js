68fc924d6e1b7dedd2faf9471c3768cbee7c59b0
Shawn Smith <srsmith11@live.com>
2021-05-04 18:10:09 -0600

    late initial commit

---
+++ api/app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const feedRoutes = require('./routes/feed');
+
+const MONGODB_URI = process.env.DATABASE_PATH
+
+const app = express();
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
+app.use('/feed', feedRoutes);
+
+mongoose.connect(
+    MONGODB_URI
+    )
+    .then(result => {
+        app.listen(8080);
+    })
+    .catch(err => console.log(err));
---
+++ api/controllers/feed.js
@@ -0,0 +1,41 @@
+require('express-validator')
+
+const Post = require('../models/post');
+
+exports.getPosts = (req, res, next) => {
+  res.status(200).json({
+    posts: [{ 
+      _id:  732989328,
+      title: 'First Post', 
+      content: 'This is the first post!', 
+      imageUrl:  'images/car.jpg',
+      creator: {
+        name: 'Shawn',
+      },
+      createdAt: new Date()
+  }]
+  });
+};
+
+exports.createPost = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ message: 'Validation failed', errors: errors.array() })
+  }
+  const title = req.body.title;
+  const content = req.body.content;
+  const post = new Post({
+      title: title,
+      content: content,
+      imageUrl: 'images/car.jpg',
+      creator: { name: 'Shawn' },
+  });
+  post.save().then(result => {
+    console.log('works"')
+    res.status(201).json({
+      message: 'Post created successfully!',
+      post: result
+    });
+  })
+  .catch(err => console.log(err))
+};
