69da4a7233c7ba24c2f4665eee4df0ffb38b443a
Shawn Smith <srsmith11@live.com>
2021-05-10 21:28:00 -0600

    linked posts with users

--- api/controllers/feed.js
+++ api/controllers/feed.js
@@ -4,6 +4,7 @@ const path = require('path');
 const { validationResult } = require('express-validator')
 
 const Post = require('../models/post');
+const User = require('../models/user');
 
 const handleError = (err, next) => {
   if (!err.statusCode) {
@@ -47,17 +48,30 @@ exports.createPost = (req, res, next) => {
   const imageUrl = req.file.path;
   const title = req.body.title;
   const content = req.body.content;
+  let creator;
   const post = new Post({
       title: title,
       content: content,
       imageUrl: imageUrl,
-      creator: { name: 'Shawn' },
+      creator: req.userId
   });
   post.save().then(result => {
-    res.status(201).json({
-      message: 'Post created successfully!',
-      post: result
-    });
+    return User.findById(req.userId);
+  })
+  .then(user => {
+    creator = user;
+    user.posts.push(post);
+    return user.save()
+      .then(result =>{
+        res.status(201).json({
+          message: 'Post created successfully!',
+          post: post,
+          creator: {
+            _id: creator._id,
+            name: creator.name
+          }
+        });
+      })
   })
   .catch(err => {
     handleError(err, next)
