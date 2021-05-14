d27c547b500819351edfab50c7106369850f443c
Shawn Smith <srsmith11@live.com>
2021-05-13 18:58:52 -0600

    added socket.io

--- api/app.js
+++ api/app.js
@@ -60,6 +60,10 @@ mongoose.connect(
     MONGODB_URI
     )
     .then(result => {
-        app.listen(8080);
+        const server = app.listen(8080);
+        const io = require('./socket').init(server);
+        io.on('connection', socket => {
+            console.log('Client connected')
+        })
     })
     .catch(err => console.log(err));
