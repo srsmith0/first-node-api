58c565b925b9739c1abc74d4582f28ce94fcb3e8
Shawn Smith <srsmith11@live.com>
2021-05-22 11:14:32 -0600

    fixed pagination bug

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -312,12 +312,14 @@ class Feed extends Component {
         };
         this.setState(prevState => {
           let updatedPosts = [...prevState.posts];
+          let updatedTotalPosts = prevState.totalPosts;
           if (prevState.editPost) {
             const postIndex = prevState.posts.findIndex(
               p => p._id === prevState.editPost._id
             );
             updatedPosts[postIndex] = post;
           } else {
+            updatedTotalPosts++;
             if (prevState.posts.length >= 2) {
               updatedPosts.pop();
             }
@@ -327,7 +329,8 @@ class Feed extends Component {
             posts: updatedPosts,
             isEditing: false,
             editPost: null,
-            editLoading: false
+            editLoading: false,
+            totalPosts: updatedTotalPosts
           };
         });
       })
